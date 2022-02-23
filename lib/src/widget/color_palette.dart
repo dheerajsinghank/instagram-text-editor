cbc1fff848ceb3dda75e62718d0097d6fb589085
Dheeraj Singh <>
2022-02-23 11:44:52 +0530

    Added the alpha slider

--- lib/src/widget/color_palette.dart
+++ lib/src/widget/color_palette.dart
@@ -14,30 +14,60 @@ class _ColorPaletteState extends State<ColorPalette> {
   @override
   Widget build(BuildContext context) {
     final textStyleModel = TextEditorData.of(context).textStyleModel;
+    final selectedColor = textStyleModel.textStyle?.color;
+
     return Container(
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          children: [
-            Container(
-              width: 40,
-              height: 40,
-              margin: EdgeInsets.only(right: 7),
-              decoration: BoxDecoration(
-                color: textStyleModel.textStyle?.color,
-                border: Border.all(color: Colors.white, width: 1.5),
-                borderRadius: BorderRadius.circular(100),
-              ),
-              child: Center(
-                child: Icon(
-                  Icons.colorize,
-                  color: Colors.white,
+      child: Column(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Container(
+                  width: 40,
+                  height: 40,
+                  margin: EdgeInsets.only(right: 7),
+                  decoration: BoxDecoration(
+                    color: selectedColor,
+                    border: Border.all(color: Colors.white, width: 1.5),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.colorize,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
-              ),
+                _ColorPicker(Colors.white),
+                _ColorPicker(Colors.black),
+                ...Colors.primaries
+                    .map((color) => _ColorPicker(color))
+                    .toList(),
+                ...Colors.accents.map((color) => _ColorPicker(color)).toList(),
+              ],
             ),
-            ...widget.colors.map((color) => _ColorPicker(color)).toList(),
-          ],
-        ),
+          ),
+          const SizedBox(height: 7),
+          Container(
+            margin: EdgeInsets.only(left: 40),
+            child: Slider(
+              value: selectedColor?.alpha.toDouble() ?? 255,
+              min: 0,
+              max: 255,
+              divisions: 255,
+              activeColor: Colors.white,
+              inactiveColor: Colors.white,
+              onChanged: (double value) {
+                print(value);
+                if (selectedColor != null) {
+                  textStyleModel
+                      .editTextColor(selectedColor.withAlpha(value.toInt()));
+                }
+              },
+            ),
+          )
+        ],
       ),
     );
   }
